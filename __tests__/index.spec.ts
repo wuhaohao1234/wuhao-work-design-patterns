d0ee50479469039d6ada2cce528fee8b21e93cda
wuhaohao1234 <1611499758@qq.com>
2019-10-20 16:48:21 +0800

    feat(添加观察者模式)

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,7 +1,45 @@
-import sum from '../src/index'
+import Observer from '../src/index'
 
-describe('sum', () => {
-    test('sum求和', () => {
-        expect(sum(1, 2)).toEqual(3)
+const observer = new Observer()
+
+describe('Observer的方法', () => {
+    test('add异步方法测试', () => {
+        const inputOne = () => {
+            console.log(1)
+        }
+        observer.add(inputOne).then((result) => {
+            expect(result).toBe(inputOne)
+        })
+    })
+    test('清空', () => {
+        observer.empty().then((result) => {
+            expect(result).toBe([])
+        })
+    })
+    test('获取所有监听的函数', () => {
+        expect(observer.getObserverList()).toEqual([])
+    })
+    test('添加函数', () => {
+        const inputOne = () => {
+            console.log(1)
+        }
+        const inputTwo = () => {
+            console.log(2)
+        }
+        observer.add(inputOne).then((result) => {
+            observer.add(inputTwo).then((resulted) => {
+                resulted.forEach((itemCallback, key) => {
+                    expect(resulted[key]).toBe(itemCallback)
+                })
+            })
+        })
+    })
+    test('去除一个', () => {
+        observer.spliceObserver(0).then((result) => {
+            expect(result.length).toBe(1)
+        })
+    })
+    test('执行', () => {
+        observer.upDate()
     })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,55 @@
-export default function sum(a: number, b: number): number {
-    return a + b
+type ICallback = () => any
+
+export default class Observer {
+    private observerList: ICallback[] = []
+    /**
+     * @param add 向观察者添加监听函数
+     * @argument callback 监听函数
+     * @returns {ICallback[]} 返回监听函数的数组
+     */
+    public add(callback: ICallback): Promise<ICallback[]> {
+        const pushObserver = () => {
+            this.observerList.push(callback)
+            return this.observerList
+        }
+        return Promise.resolve(pushObserver())
+    }
+    /**
+     * @param empty 清空监听函数
+     * @returns {ICallback[]} 返回监听函数的数组
+     */
+    public empty(): Promise<ICallback[]> {
+        this.observerList = []
+        return Promise.resolve(this.observerList)
+    }
+    /**
+     * @param getObserverList 获取所有监听的函数
+     */
+    public getObserverList(): ICallback[] {
+        return this.observerList
+    }
+    /**
+     * @argument key 去除索引为number
+     * @param spliceObserver
+     * @returns {ICallback[]}
+     */ 
+    public spliceObserver(key: number): Promise<ICallback[]> {
+        if (this.observerList[key]) {
+            this.observerList.splice(0, key)
+        }
+        return Promise.resolve(this.observerList)
+    }
+    /**
+     * @param upDate 执行所有监听的函数
+     * @returns {ICallback[]} 返回监听函数的数组
+     */
+    public upDate(): Promise<ICallback[]> | Promise<string> {
+        if (this.observerList.length === 0) {
+            return Promise.resolve('没有监听的函数')
+        }
+        this.observerList.forEach((itemCallback: ICallback) => {
+            itemCallback()
+        })
+        return Promise.resolve(this.observerList)
+    }
 }
